e8b0ea6348698ab6ac9bb91636647b5597590403
Aldi Alimucaj <aldi@outlook.com>
2013-11-16 01:49:51 +0100

    [FEATURE] Random alpha-numeric String

--- libjaldi/src/al/aldi/libjaldi/string/AldiStringUtils.java
+++ libjaldi/src/al/aldi/libjaldi/string/AldiStringUtils.java
@@ -1,10 +1,12 @@
 package al.aldi.libjaldi.string;
 
+import java.util.Random;
+
 public class AldiStringUtils {
 
     /**
      * Joins the array into a single string.
-     * 
+     *
      * @param strArray
      * @return joined array
      */
@@ -24,7 +26,7 @@ public class AldiStringUtils {
     /**
      * Joins the array with a suffix
      *
-     * @param strArray the array to be joined
+     * @param strArray  the array to be joined
      * @param separator - the suffix to be added after the token. It is not added in the last token.
      * @return joined array
      */
@@ -44,8 +46,8 @@ public class AldiStringUtils {
     /**
      * Joins the array with a prefix and suffix
      *
-     * @param strArray the array to be joined
-     * @param prefix - the prefix to be added before every array token is joined
+     * @param strArray  the array to be joined
+     * @param prefix    - the prefix to be added before every array token is joined
      * @param separator - the suffix to be added after the token. It is not added in the last token.
      * @return joined array
      */
@@ -62,12 +64,12 @@ public class AldiStringUtils {
         return concatString;
 
     }
-    
+
     /**
      * Checks if the string starts with HTTP or HTTPS
-     * 
+     * <p/>
      * Like http:// or https://
-     * 
+     *
      * @param string
      * @return
      */
@@ -82,7 +84,7 @@ public class AldiStringUtils {
      * @return true if null or empty
      */
     public static boolean isNullOrEmpty(String str) {
-        if(null != str) {
+        if (null != str) {
             return str.equals("");
         }
         return true;
@@ -90,9 +92,10 @@ public class AldiStringUtils {
 
     /**
      * Adding rounded brackets to string and returning it
-     * @code AldiStringUtils.addRoundBrackets("name"); // => (name)
+     *
      * @param str String to be put between brackets
      * @return concatenated string with brackets
+     * @code AldiStringUtils.addRoundBrackets("name"); // => (name)
      */
     public static String addRoundBrackets(String str) {
         return "(" + str + ")";
@@ -100,11 +103,43 @@ public class AldiStringUtils {
 
     /**
      * Adding rounded brackets to string and returning it
-     * @code AldiStringUtils.addSquareBrackets("name"); // => [name]
+     *
      * @param str String to be put between brackets
      * @return concatenated string with brackets
+     * @code AldiStringUtils.addSquareBrackets("name"); // => [name]
      */
     public static String addSquareBrackets(String str) {
         return "[" + str + "]";
+    }
+
+    /* ********************************************************************** */
+
+    public static String random(int length) {
+        return new RandomString(length).nextString();
+    }
+
+    static class RandomString {
+
+        private final char[] symbols = new char[36];
+        private final Random random = new Random();
+        private final char[] buf;
+
+        public RandomString(int length) {
+            for (int idx = 0; idx < 10; ++idx)
+                symbols[idx] = (char) ('0' + idx);
+            for (int idx = 10; idx < 36; ++idx)
+                symbols[idx] = (char) ('a' + idx - 10);
+
+            if (length < 1)
+                throw new IllegalArgumentException("str length < 1: " + length);
+            buf = new char[length];
+        }
+
+        public String nextString() {
+            for (int idx = 0; idx < buf.length; ++idx)
+                buf[idx] = symbols[random.nextInt(symbols.length)];
+            return new String(buf);
+        }
+
     }
 }
